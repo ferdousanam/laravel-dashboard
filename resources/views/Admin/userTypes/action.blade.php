a3d6cbd6a27401b8baabe4e5cec68b6e8ede6ddf
ferdousanam <ferdous.anam@gmail.com>
2019-10-31 12:12:25 +0600

    Delete Modal Added in Delete User Type Button

--- resources/views/Admin/userTypes/action.blade.php
+++ resources/views/Admin/userTypes/action.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ route('user-type.edit', $id) }}" class="btn btn-sm btn-clean btn-icon btn-icon-md" title="View">
   <i class="la la-edit"></i>
 </a>
-<button class="btn btn-sm btn-clean btn-icon btn-icon-md" onclick="Delete({{ $id }})">
+<button type="button" class="btn btn-sm btn-clean btn-icon btn-icon-md delete-button" data-toggle="modal" data-target="#delete-modal" data-id="{{ $id }}">
   <i class="fa fa-trash lg"></i>
 </button>

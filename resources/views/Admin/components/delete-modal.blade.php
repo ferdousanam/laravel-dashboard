2d458b01d9ef4bd91a655cf0c549087f05b72d17
ferdousanam <ferdous.anam@gmail.com>
2019-10-31 12:04:34 +0600

    Delete Modal Added in Delete User Button

---
+++ resources/views/Admin/components/delete-modal.blade.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="delete-modal" tabindex="-1" role="dialog" aria-labelledby="deleteModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="deleteModalTitle">Delete Alert</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Are you sure you want to delete?</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal" id="modal-delete-button">Delete</button>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ resources/views/Admin/scripts/delete.blade.php
@@ -0,0 +1,29 @@
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('table').on('click', '.delete-button', function () {
+            let id = $(this).attr('data-id');
+            $('#modal-delete-button').unbind().click(function () {
+                $.ajax({
+                    headers: {'X-CSRF-TOKEN': '{{ csrf_token() }}'},
+                    url: "{{ url()->current() }}/" + id,
+                    type: 'DELETE',
+                    dataType: 'json',
+                    data: {},
+                    success: function (response) {
+                        console.log(response);
+                        if (response.success) {
+                            $('#tr-' + id).fadeOut();
+                        } else {
+                            toastr.error("Whoops! Something Went Wrong!")
+                        }
+                    }
+                }).done(function () {
+
+                });
+            });
+        });
+
+        $('#user-mm').addClass('kt-menu__item--submenu kt-menu__item--open kt-menu__item--here');
+        $('#user-sm').addClass('kt-menu__item--active');
+    });
+</script>

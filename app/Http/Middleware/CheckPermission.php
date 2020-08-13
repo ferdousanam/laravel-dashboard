91789677a33f7143ef7b4c227143f8e0d27ceda0
Ferdous Anam <ferdous.anam@gmail.com>
2020-08-13 18:33:52 +0600

    If Admin login do not check permissions

--- app/Http/Middleware/CheckPermission.php
+++ app/Http/Middleware/CheckPermission.php
@@ -8,26 +8,29 @@ use Illuminate\Support\Facades\DB;
 
 class CheckPermission
 {
-  /**
-   * Handle an incoming request.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  \Closure  $next
-   * @return mixed
-   */
-  public function handle($request, Closure $next)
-  {
-    $route_path = $request->path();
-    $getMenuId = Menu::where('route_name', $route_path)->first();
-    if (isset($getMenuId->id)) {
-      $has_permission = DB::table('priority_menu')->where('priority_id', $request->user()->user_level)
-        ->where('menu_id', $getMenuId->id)->first();
-    } else {
-      $has_permission = false;
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->user()->email == 'admin@gmail.com') {
+            return $next($request);
+        }
+        $route_path = $request->path();
+        $getMenuId = Menu::where('route_name', $route_path)->first();
+        if (isset($getMenuId->id)) {
+            $has_permission = DB::table('priority_menu')->where('priority_id', $request->user()->user_level)
+                ->where('menu_id', $getMenuId->id)->first();
+        } else {
+            $has_permission = false;
+        }
+        if (!$has_permission) {
+            return redirect(route('dashboard.index'));
+        }
+        return $next($request);
     }
-    if (!$has_permission) {
-      return redirect(route('dashboard.index'));
-    }
-    return $next($request);
-  }
 }
